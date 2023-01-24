808c7aaed52578e619193995e572df768eca289e
keongmini <kmnam09@gmail.com>
2023-01-24 23:45:47 +0900

    boards service controller and model

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { Board } from './boards.model';
+import { BoardsService } from './boards.service';
+
+@Controller('boards')
+export class BoardsController {
+  constructor(private boardsService: BoardsService) {} // dependency injection
+
+  @Get() // handler
+  getAllBoard(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+}
